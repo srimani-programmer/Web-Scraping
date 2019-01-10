d8d543f0bad5ad1036aec4dfd66bf938e1a2c839
srimani-programmer <srimani.scholar@gmail.com>
2019-01-10 07:31:14 +0530

    Code Modified.

--- Wikipedia/Machine Learning Wiki/Scrapy.py
+++ Wikipedia/Machine Learning Wiki/Scrapy.py
@@ -5,7 +5,7 @@ file = open('Data.txt','wb')
 req = requests.get('https://en.wikipedia.org/wiki/Machine_learning')
 
 soup_obj = bs4.BeautifulSoup(req.text,'lxml')
-paragraph = soup_obj.select('a')
+paragraph = soup_obj.select('p')
 for i in paragraph:
     data = i.get_text()
     file.write(data.encode())
